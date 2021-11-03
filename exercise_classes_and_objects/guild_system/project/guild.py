253dc4e57e1069e1f77d7d57dabdf6a4b8fe02b1
gyurel <gyurel@yahoo.com>
2021-11-03 16:08:07 +0100

    Problem 6

---
+++ exercise_classes_and_objects/guild_system/project/guild.py
@@ -0,0 +1,38 @@
+from guild_system.project.player import Player
+
+
+class Guild:
+
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+
+        if player.guild == 'Unaffiliated':
+            self.players.append(player)
+            player.guild = self.name
+            return f"Welcome player {player.name} to the guild {self.name}"
+
+        elif player in self.players:
+            return f"Player {player.name} is already in the guild."
+
+        else:
+            return f"Player {player.name} is in another guild."
+
+    def kick_player(self, player_name):
+        for player in self.players:
+            if player.name == player_name:
+                player.guild = 'Unaffiliated'
+                self.players.remove(player)
+                return f"Player {player.name} has been removed from the guild."
+
+        return f"Player {player_name} is not in the guild."
+
+    def guild_info(self):
+        info = f"Guild: {self.name}\n"
+
+        for player in self.players:
+            info += f"{player.player_info()}\n"
+
+        return info
