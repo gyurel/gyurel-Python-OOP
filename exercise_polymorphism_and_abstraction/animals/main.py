31e806f7584e4bc9db3aeefb29818df878d12757
gyurel <gyurel@yahoo.com>
2021-11-16 23:31:54 +0100

    Problem 5

---
+++ exercise_polymorphism_and_abstraction/animals/main.py
@@ -0,0 +1,9 @@
+from animals.project.cat import Cat
+from animals.project.kitten import Kitten
+
+kitten = Kitten("Kiki", 1)
+print(kitten.make_sound())
+print(kitten)
+cat = Cat("Johnny", 7, "Male")
+print(cat.make_sound())
+print(cat)
---
+++ exercise_polymorphism_and_abstraction/animals/project/tomcat.py
@@ -0,0 +1,9 @@
+from animals.project.cat import Cat
+
+
+class Tomcat(Cat):
+    def __init__(self, name, age):
+        super().__init__(name, age, "Male")
+
+    def make_sound(self):
+        return "Hiss"
