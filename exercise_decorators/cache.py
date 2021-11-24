348f5ffa003b653645910a547609d80799a93edb
gyurel <gyurel@yahoo.com>
2021-11-24 16:44:25 +0100

    Problem 5

---
+++ exercise_decorators/cache.py
@@ -0,0 +1,27 @@
+def cache(func):
+    log = {}
+
+    def wrapper(n):
+        if n in log:
+            return log[n]
+        current_fib_num = func(n)
+        log[n] = current_fib_num
+        return current_fib_num
+
+    wrapper.log = log
+    return wrapper
+
+
+@cache
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+fibonacci(3)
+print(fibonacci.log)
+
+fibonacci(4)
+print(fibonacci.log)
